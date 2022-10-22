35ee88d51bacd7462cf45b706175d96d1e70ce8e
kadeem-lewis <print.helloworld@genericindividual.com>
2022-10-22 16:35:00 -0400

    fixed webpack setup

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,5 @@ import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import scrollGridPlugin from '@fullcalendar/scrollgrid';
+
+import "../css/styles.scss";
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,55 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const path = require('path');
 
 module.exports = {
-  module: {
-    rules: [
-        {
-            test: /\.html$/, 
-            use: [
-                {
-                    loader: "html-loader",
-                    options: {minimize: true } 
-                }
-            ]
-        }
+    entry: "./src/js/index.js",
+    output: {
+        path: path.resolve(__dirname,"dist"),
+        filename: 'bundle.js',
+        publicPath: "/dist"
+    },
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.html$/, 
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: {minimize: true } 
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: MiniCssExtractPlugin.loader },
+                    { loader: 'css-loader', options: { importLoaders: 1 } }
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                // fallback to style-loader in development
+                process.env.NODE_ENV !== "production"
+                    ? "style-loader"
+                    : MiniCssExtractPlugin.loader,
+                "css-loader",
+                "sass-loader",
+                ],
+            },
+            ],
+        },
+    plugins: [
+        new HtmlWebPackPlugin(),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "[name].css",
+            chunkFilename: "[id].css",
+          }),
     ],
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-        template: "index.html",
-        filename: "index.html"
-    }),
-  ],
+    
 };
