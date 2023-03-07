c9b93b737e31b4048a57ed7900b4f2f7a76be39e
kadeem-lewis <print.helloworld@genericindividual.com>
2023-03-07 01:12:01 -0500

    began defining react router paths and files

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
+import { Route, Routes } from "react-router";
+
+import { MyDay } from "./pages/MyDay";
+import { TaskBoard } from "./pages/TaskBoard";
+import { NotFound } from "./pages/NotFound";
+import { SignUp } from "./pages/SignUp";
+import { SignIn } from "./pages/SignIn";
+
 export const App = () => {
-  return <div className="App">Hey</div>;
+  return (
+    <Routes>
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/" element={<MyDay />} />
+      <Route path="/" element={<TaskBoard />} />
+    </Routes>
+  );
 };
