dda43a0f8564f8317dcf6a72954887e9e37f0b69
kadeem-lewis <print.helloworld@genericindividual.com>
2023-03-11 15:09:09 -0500

    updated route structure and imports in app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import { Route, Routes } from "react-router";
 
-import { MyDay } from "./pages/MyDay";
-import { TaskBoard } from "./pages/TaskBoard";
 import { NotFound } from "./pages/NotFound";
 import { SignUp } from "./pages/SignUp";
 import { SignIn } from "./pages/SignIn";
+import { LandingPage } from "./pages/LandingPage";
+import { AppRoutes } from "./routes/AppRoutes";
 
 export const App = () => {
   return (
     <Routes>
+      <Route path="/home" element={<LandingPage />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/signin" element={<SignIn />} />
-      <Route path="/" element={<MyDay />} />
-      <Route path="/" element={<TaskBoard />} />
+      <Route path="/app/*" element={<AppRoutes />} />
+      <Route path="/*" element={<NotFound />} />
     </Routes>
   );
 };
