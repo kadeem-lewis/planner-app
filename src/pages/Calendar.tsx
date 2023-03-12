81b4e358ec7b300ec95bb3184419d44d3476f0b3
kadeem-lewis <print.helloworld@genericindividual.com>
2023-03-12 00:26:52 -0500

    added full calendar calender to calendar route

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import timeGridPlugin from "@fullcalendar/timegrid";
 
 export const Calendar = () => {
-  return <div>Calendar</div>;
+  return (
+    <div>
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+        initialView="dayGridMonth"
+        headerToolbar={{
+          left: "prev,next today",
+          center: "title",
+          right: "dayGridMonth,timeGridWeek,timeGridDay",
+        }}
+      />
+    </div>
+  );
 };
