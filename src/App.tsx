c51970c62394c920125267283bf7b31ec8e25bed
kadeem-lewis <print.helloworld@genericindividual.com>
2023-03-18 02:12:04 -0400

    imported auth layouts and wrapped app in auth provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
 import { Route, Routes } from "react-router";
 
 import { NotFound } from "./pages/NotFound";
-import { SignUp } from "./pages/SignUp";
-import { SignIn } from "./pages/SignIn";
-import { LandingPage } from "./pages/LandingPage";
+import { SignUp } from "./pages/Auth/SignUp";
+import { LogIn } from "./pages/Auth/LogIn";
+import { LandingPage } from "./pages/Home/LandingPage";
 import { AppRoutes } from "./routes/AppRoutes";
+import { HomeLayout } from "./layouts/HomeLayout";
+import { AuthRoutes } from "./routes/AuthRoutes";
+import { AuthProvider } from "./contexts/AuthContext";
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/home" element={<LandingPage />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/app/*" element={<AppRoutes />} />
-      <Route path="/*" element={<NotFound />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route element={<HomeLayout />}>
+          <Route path="/home" element={<LandingPage />} />
+        </Route>
+        <Route path="/auth/*" element={<AuthRoutes />} />
+        <Route path="/app/*" element={<AppRoutes />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+    </AuthProvider>
   );
 };
