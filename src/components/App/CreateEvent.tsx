b7b41917f657ae6d6858ab4acb7ad6fec85c6f16
kadeem-lewis <print.helloworld@genericindividual.com>
2023-03-19 22:47:23 -0400

    create task and events tabs can add data to firestore

--- src/components/App/CreateEvent.tsx
+++ src/components/App/CreateEvent.tsx
@@ -1,21 +1,49 @@
-import React, { Dispatch } from "react";
-
+import React, { Dispatch, useRef, FormEvent } from "react";
+import { Timestamp } from "firebase/firestore";
+import { useFireStore } from "../../contexts/FirestoreContext";
 interface Props {
   setIsOpen: Dispatch<React.SetStateAction<boolean>>;
 }
 export const CreateEvent = ({ setIsOpen }: Props) => {
+  const { addEvent } = useFireStore();
+  const titleRef = useRef<HTMLInputElement>(null);
+  const startDateRef = useRef<HTMLInputElement>(null);
+  const endDateRef = useRef<HTMLInputElement>(null);
+
+  const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (
+      titleRef.current &&
+      endDateRef.current &&
+      startDateRef.current &&
+      addEvent
+    ) {
+      try {
+        await addEvent(
+          titleRef.current.value,
+          startDateRef.current.value,
+          endDateRef.current.value
+        );
+        setIsOpen(false);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  };
   return (
-    <form onSubmit={() => setIsOpen(false)} className="form-control">
+    <form onSubmit={(e) => handleFormSubmit(e)} className="form-control">
       <label htmlFor="title" className="label">
         Title:
       </label>
-      <input type="text" name="title" className="input" />
-      <label htmlFor="description">Description</label>
-      <textarea
-        name="description"
-        placeholder="Description"
-        className="textarea"
-      ></textarea>
+      <input type="text" name="title" className="input" ref={titleRef} />
+      <label htmlFor="start-time" className="label">
+        Start
+      </label>
+      <input type="datetime-local" name="start-time" ref={startDateRef} />
+      <label htmlFor="end-time" className="label">
+        End
+      </label>
+      <input type="datetime-local" name="end-time" ref={endDateRef} />
       <input type="submit" value="Add" className="btn" />
     </form>
   );
