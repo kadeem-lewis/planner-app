9000c2d8bd8c6e4c91ef118add02ede0b9ad4293
kadeem-lewis <print.helloworld@genericindividual.com>
2023-03-19 22:46:49 -0400

    wrapped all app routes in firestore context

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,15 +4,18 @@ import { Calendar } from "../pages/App/Calendar";
 import { Today } from "../pages/App/Today";
 import { TaskBoard } from "../pages/App/TaskBoard";
 import { AppLayout } from "../layouts/AppLayout";
+import { FirestoreProvider } from "../contexts/FirestoreContext";
 
 export const AppRoutes = () => {
   return (
-    <Routes>
-      <Route element={<AppLayout />}>
-        <Route path="calendar" element={<Calendar />} />
-        <Route path="today" element={<Today />} />
-        <Route path="taskboard" element={<TaskBoard />} />
-      </Route>
-    </Routes>
+    <FirestoreProvider>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route path="calendar" element={<Calendar />} />
+          <Route path="today" element={<Today />} />
+          <Route path="taskboard" element={<TaskBoard />} />
+        </Route>
+      </Routes>
+    </FirestoreProvider>
   );
 };
