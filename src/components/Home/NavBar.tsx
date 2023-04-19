34f96e9cd9894700853936509a5f317217d9de96
kadeem-lewis <print.helloworld@genericindividual.com>
2023-04-19 16:36:28 -0400

    replaced react router links with next link

--- src/components/Home/NavBar.tsx
+++ src/components/Home/NavBar.tsx
@@ -1,20 +1,19 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-
-export const NavBar = () => {
+import Link from "next/link";
+export default function NavBar() {
   return (
     <header>
       <nav className="flex justify-between bg-base-200 navbar">
-        <Link to="/home">Taskbored</Link>
+        <Link href="/">Taskbored</Link>
         <div>
           <button className="btn mr-4">
-            <Link to="auth/signup">Signup</Link>
+            <Link href="auth/signup">Signup</Link>
           </button>
           <button className="btn">
-            <Link to="auth/login">Login</Link>
+            <Link href="auth/login">Login</Link>
           </button>
         </div>
       </nav>
     </header>
   );
-};
+}
