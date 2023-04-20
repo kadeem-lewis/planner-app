1de78fa5818789e2adcfc3b870e7aa59606fd954
kadeem-lewis <print.helloworld@genericindividual.com>
2023-04-19 23:31:24 -0400

    updated config file to use next env names

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.VITE_API_KEY,
-  authDomain: process.env.VITE_AUTH_DOMAIN,
-  projectId: process.env.VITE_PROJECT_ID,
-  storageBucket: process.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VITE_MESSAGE_SENDER_ID,
-  appId: process.env.VITE_APP_ID,
-  measurementId: process.env.VITE_MEASUREMENT_ID,
+  apiKey: process.env.NEXT_API_KEY,
+  authDomain: process.env.NEXT_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PROJECT_ID,
+  storageBucket: process.env.NEXT_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_MESSAGE_SENDER_ID,
+  appId: process.env.NEXT_APP_ID,
+  measurementId: process.env.NEXT_MEASUREMENT_ID,
 };
 const app = initializeApp(firebaseConfig);
 
