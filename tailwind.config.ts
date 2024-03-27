17b7cefe0cc8711fdd71c6f5a7a7c4aaea8f8a67
kadeem-lewis <email@kadeemlewis.com>
2024-03-26 22:47:17 -0400

    updated files to reference remix instead of nextjs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/app/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
