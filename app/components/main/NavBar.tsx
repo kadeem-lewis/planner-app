46c18300949f087bc5f4d79ab18d1bda5d704461
kadeem-lewis <email@kadeemlewis.com>
2024-03-27 18:05:15 -0400

    converting navbar and sidebar to be styling using tailwind and shadcn instead of daisyui

--- app/components/main/NavBar.tsx
+++ app/components/main/NavBar.tsx
@@ -1,17 +1,20 @@
 import { useState } from "react";
-import { Home, Bell, Menu } from "lucide-react";
+import { Home, Bell, Menu, Calendar, CalendarDays, KanbanSquare } from "lucide-react";
 import ActivityButton from "./ActivityButton";
 import NotificationTab from "./NotificationTab";
-import { Link, useNavigate } from "@remix-run/react";
+import { Link, NavLink, useNavigate } from "@remix-run/react";
 import { useAuth } from "~/contexts/AuthContext";
 import { Avatar, AvatarFallback } from "../ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
 import { Button, buttonVariants } from "../ui/button";
+import { Sheet,SheetContent,SheetTrigger } from "../ui/sheet";
 
 export default function NavBar() {
   const { currentUser, logout } = useAuth();
@@ -31,12 +34,23 @@ export default function NavBar() {
   };
 
   return (
-    <header className="bg-muted/40 border-b">
+    <header className="bg-muted/40 border-b h-14 flex items-center gap-4 px-4 lg:px-6">
       <nav className="navbar flex justify-between">
         <div id="nav-left" className="gap-x-1">
-          <Button variant="ghost">
+        <Sheet>
+        <SheetTrigger asChild>
+          <Button variant="outline" size="icon" className="shrink-0 md:hidden">
             <Menu />
           </Button>
+        </SheetTrigger>
+        <SheetContent side="left" className="flex flex-col">
+          <nav className="grid font-medium gap-2 p-4">
+            <NavLink to="/app/today" className="flex items-center gap-4 rounded-xl text-lg font-semibold text-muted-foreground hover:text-foreground"><Calendar/>Today</NavLink>
+            <NavLink to="/app/calendar" className="flex items-center gap-4 rounded-xl text-lg font-semibold text-muted-foreground hover:text-foreground"><CalendarDays/>Calendar</NavLink>
+            <NavLink to="/app/taskboard" className="flex items-center gap-4 rounded-xl text-lg font-semibold text-muted-foreground hover:text-foreground"><KanbanSquare/>Taskboard</NavLink>
+          </nav>
+        </SheetContent>
+      </Sheet>
           <Link
             to="/app/today"
             className={buttonVariants({ variant: "ghost" })}
@@ -67,12 +81,15 @@ export default function NavBar() {
               </Avatar>
             </DropdownMenuTrigger>
             <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
               <DropdownMenuItem>
                 <Link to="#">Profile</Link>
               </DropdownMenuItem>
               <DropdownMenuItem>
                 <Link to="#">Settings</Link>
               </DropdownMenuItem>
+              <DropdownMenuSeparator />
               <DropdownMenuItem>
                 <Link to="#" onClick={() => handleLogOut()}>
                   Logout
--- app/components/main/SideBar.tsx
+++ app/components/main/SideBar.tsx
@@ -1,25 +1,29 @@
 import { Calendar, CalendarDays, SquareKanban } from "lucide-react";
-import { Link } from "@remix-run/react";
+import {  NavLink } from "@remix-run/react";
 
 export default function SideBar() {
   return (
-    <aside className="menu col-span-2 bg-muted/40 border-r">
-      <ul>
-        <li>
-          <Link to="/app/today">
-            <Calendar />
-            Today
-          </Link>
-          <Link to="/app/calendar">
-            <CalendarDays />
-            Calendar
-          </Link>
-          <Link to="/app/taskboard">
-            <SquareKanban />
-            Taskboard
-          </Link>
-        </li>
-      </ul>
+    <aside className="hidden md:flex col-span-2 py-2 px-4 flex-col bg-muted/40 border-r">
+      <nav className="font-medium">
+          <NavLink to="/app/today" className="flex items-center px-4 py-2 gap-2 text-muted-foreground hover:text-primary mt-5 rounded-lg transition-all ">
+            <Calendar className="size-5"/>
+            <span className="font-medium">
+              Today
+              </span>
+          </NavLink>
+          <NavLink to="/app/calendar" className="flex items-center px-4 py-2 gap-2 text-muted-foreground hover:text-primary mt-5 rounded-lg transition-all">
+            <CalendarDays className="size-5"/>
+            <span className="font-medium">
+              Calendar
+            </span>
+          </NavLink>
+          <NavLink to="/app/taskboard" className="flex items-center px-4 py-2 gap-2 text-muted-foreground hover:text-primary mt-5 rounded-lg transition-all">
+            <SquareKanban className="size-5"/>
+            <span className="font-medium">
+              Taskboard
+            </span>
+          </NavLink>
+      </nav>
     </aside>
   );
 }
