c99830d1ca0ff2074bf038fbf14da57bed599283
kadeem-lewis <email@kadeemlewis.com>
2024-03-27 21:45:28 -0400

    updated code to use postcss from vite and not separately install package

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,19 @@ import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+import tailwind from "tailwindcss"
+import autoprefixer from "autoprefixer"
+
 installGlobals();
 
 export default defineConfig({
   plugins: [remix(), tsconfigPaths()],
+  css:{
+    postcss:{
+      plugins:[
+        tailwind(),
+        autoprefixer()
+      ]
+    }
+  }
 });
