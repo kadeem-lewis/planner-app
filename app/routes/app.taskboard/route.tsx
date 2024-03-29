0b60be537325707e4215d5bd3ac30cac59e675cf
kadeem-lewis <email@kadeemlewis.com>
2024-03-28 21:00:29 -0400

    rendering kanban board in taskboard route

--- app/routes/app.taskboard/route.tsx
+++ app/routes/app.taskboard/route.tsx
@@ -1,3 +1,23 @@
+import KanbanBoard from "./KanbanBoard";
+import { useFireStore } from "~/contexts/FirestoreContext"
+import { DndContext } from "@dnd-kit/core";
+
+const boards = [ "Not Started", "In Progress", "Completed"]
+
 export default function TaskBoard() {
-  return <div>TaskBoard</div>;
+
+  const {tasks} = useFireStore();
+
+
+  return <div>
+    <DndContext>
+    <div className="flex gap-4">
+      {boards.map((board) => {
+        return <KanbanBoard name={board} key={board} activities={tasks?.filter(task=>task.progress===board)}/>
+
+      }
+      )}
+    </div>
+    </DndContext>
+  </div>;
 }
