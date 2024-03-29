ae6a7afc4ccc48e9861004980548640d393356d4
kadeem-lewis <email@kadeemlewis.com>
2024-03-29 02:51:57 -0400

    styling card to better match desired look

--- app/components/main/ActivityCard.tsx
+++ app/components/main/ActivityCard.tsx
@@ -1,10 +1,13 @@
+import { CalendarClock, Calendar, LayoutList } from "lucide-react";
 import {
   Card,
   CardContent,
   CardHeader,
   CardTitle,
+  CardDescription,
   CardFooter,
 } from "../ui/card";
+import { format, isSameDay } from "date-fns";
 
 type ActivityCardProps = {
   activity: Record<string, string>;
@@ -14,9 +17,31 @@ export default function ActivityCard({ activity }: ActivityCardProps) {
   return (
     <Card>
       <CardHeader>
-        <CardTitle>{activity.title}</CardTitle>
+        <CardTitle className="text-xl">{activity.title}</CardTitle>
+        {activity.description && (
+          <CardDescription>{activity.description}</CardDescription>
+        )}
       </CardHeader>
-      <CardContent></CardContent>
+      <CardContent className="flex gap-4">
+        {activity.dueDate && (
+          <div className="flex gap-2 text-muted-foreground">
+            <CalendarClock />
+            <p>{format(activity.dueDate, "MMM d, yyyy")}</p>
+          </div>
+        )}
+        {activity.subtasks && (
+          <div className="flex gap-2">
+            <LayoutList />
+            <span>
+              {
+                activity.subtasks.filter((task) => task.status === "completed")
+                  .length
+              }
+              /{activity.subtasks.length}
+            </span>
+          </div>
+        )}
+      </CardContent>
     </Card>
   );
 }
