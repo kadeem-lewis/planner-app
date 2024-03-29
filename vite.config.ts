c431f62c314b865d5787240034b4c6ae49e3965f
kadeem-lewis <email@kadeemlewis.com>
2024-03-29 07:44:33 -0400

    added and completed initial vite-pwa/remix setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,27 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
+import { RemixVitePWA } from "@vite-pwa/remix";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-import tailwind from "tailwindcss"
-import autoprefixer from "autoprefixer"
+import tailwind from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 installGlobals();
 
+const { RemixVitePWAPlugin, RemixPWAPreset } = RemixVitePWA();
+
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
-  css:{
-    postcss:{
-      plugins:[
-        tailwind(),
-        autoprefixer()
-      ]
-    }
-  }
+  plugins: [
+    remix({
+      presets: [RemixPWAPreset()],
+    }),
+    tsconfigPaths(),
+    RemixVitePWAPlugin({}),
+  ],
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
 });
