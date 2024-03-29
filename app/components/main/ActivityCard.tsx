2e8186562498f57d0e3998632364c58f354f5288
kadeem-lewis <email@kadeemlewis.com>
2024-03-28 20:59:57 -0400

    created cards for items that will be in kanban board

---
+++ app/components/main/ActivityCard.tsx
@@ -0,0 +1,17 @@
+import { Card,CardContent,CardHeader,CardTitle,CardFooter } from "../ui/card"
+
+
+type ActivityCardProps = {
+    activity: Record<string,string>
+    }
+
+export default function ActivityCard({activity}:ActivityCardProps) {
+  return (
+    <Card>
+        <CardHeader>
+            <CardTitle>{activity.title}</CardTitle>
+        </CardHeader>
+        <CardContent></CardContent>
+        </Card>
+  )
+}
