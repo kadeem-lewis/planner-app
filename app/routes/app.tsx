c4bacff77efc0c201182b12f4cfba790faf255c4
kadeem-lewis <email@kadeemlewis.com>
2024-03-31 07:55:55 -0400

    fixed using incorrect package for routing

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from "@remix-run/react";
 import NavBar from "~/components/main/NavBar";
 import SideBar from "~/components/main/SideBar";
 import { FirestoreProvider } from "~/contexts/FirestoreContext";
