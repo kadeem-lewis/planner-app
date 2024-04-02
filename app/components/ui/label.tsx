b2b59830d76633ca4f15fe6cf634fd91b871701e
kadeem-lewis <email@kadeemlewis.com>
2024-04-02 03:57:18 -0400

    removed shadcn form components in favor of jollyui

--- app/components/ui/label.tsx
+++ app/components/ui/label.tsx
@@ -1,24 +1,14 @@
-import * as React from "react"
-import * as LabelPrimitive from "@radix-ui/react-label"
-import { cva, type VariantProps } from "class-variance-authority"
+import { cva } from "class-variance-authority";
+import { Label, LabelProps } from "react-aria-components";
 
-import { cn } from "~/lib/utils"
+import { cn } from "~/lib/utils";
 
 const labelVariants = cva(
-  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-)
+  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 data-[disabled]:cursor-not-allowed data-[disabled]:opacity-70",
+);
 
-const Label = React.forwardRef<
-  React.ElementRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
-    VariantProps<typeof labelVariants>
->(({ className, ...props }, ref) => (
-  <LabelPrimitive.Root
-    ref={ref}
-    className={cn(labelVariants(), className)}
-    {...props}
-  />
-))
-Label.displayName = LabelPrimitive.Root.displayName
+const _Label = ({ className, ...props }: LabelProps) => (
+  <Label className={cn(labelVariants(), className)} {...props} />
+);
 
-export { Label }
+export { _Label as Label, labelVariants };
