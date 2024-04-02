7a8212879a86ce5d53bf41a961ba215b08cc07db
kadeem-lewis <email@kadeemlewis.com>
2024-04-02 03:46:36 -0400

    configured app to use clerk

--- app/root.tsx
+++ app/root.tsx
@@ -9,7 +9,7 @@ import {
   useNavigate,
   useRouteError,
 } from "@remix-run/react";
-import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
+import type { LinksFunction, LoaderFunction } from "@remix-run/node";
 import stylesheet from "~/tailwind.css?url";
 import { AuthProvider } from "./contexts/AuthContext";
 import {
@@ -19,20 +19,24 @@ import {
 } from "remix-themes";
 import { themeSessionResolver } from "./sessions.server";
 import { RouterProvider } from "react-aria-components";
+import { rootAuthLoader } from "@clerk/remix/ssr.server";
+import { ClerkApp, ClerkErrorBoundary } from "@clerk/remix";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
 ];
 
 // Return the theme from the session storage using the loader
-export async function loader({ request }: LoaderFunctionArgs) {
-  const { getTheme } = await themeSessionResolver(request);
-  return {
-    theme: getTheme(),
-  };
-}
+export const loader: LoaderFunction = (args) => {
+  return rootAuthLoader(args, async ({ request }) => {
+    const { getTheme } = await themeSessionResolver(request);
+    return {
+      theme: getTheme(),
+    };
+  });
+};
 
-export function App() {
+function App() {
   const data = useLoaderData<typeof loader>();
   const [theme] = useTheme();
 
@@ -56,13 +60,13 @@ export function App() {
   );
 }
 
-export const ErrorBoundary = () => {
+export const ErrorBoundary = ClerkErrorBoundary(() => {
   const error = useRouteError();
   captureRemixErrorBoundaryError(error);
   return <div>Something went wrong</div>;
-};
+});
 
-export default function AppWithProviders() {
+function AppWithProviders() {
   const data = useLoaderData<typeof loader>();
   const navigate = useNavigate();
   return (
@@ -73,3 +77,5 @@ export default function AppWithProviders() {
     </ThemeProvider>
   );
 }
+
+export default ClerkApp(AppWithProviders);
