aec26258841d49fe898767376a4e5ffc0421e0b3
kadeem-lewis <email@kadeemlewis.com>
2024-04-02 16:41:18 -0400

    added first instance of using drizzle instead of firebase

--- app/routes/app.taskboard/route.tsx
+++ app/routes/app.taskboard/route.tsx
@@ -1,12 +1,24 @@
 import { useState } from "react";
 import KanbanBoard from "./KanbanBoard";
-import { useFireStore } from "~/contexts/FirestoreContext";
+import { LoaderFunction, json } from "@remix-run/node";
+import { db } from "~/drizzle/config.server";
+import { tasks } from "~/drizzle/schema.server";
+import { eq } from "drizzle-orm";
+import { getAuth } from "@clerk/remix/ssr.server";
+import { useLoaderData } from "@remix-run/react";
+
+export const loader: LoaderFunction = async (args) => {
+  const { userId } = await getAuth(args);
+
+  //! Not sure if using ! is recommended but userId shouldn't be able to be null since it is handled in the layout
+  return json(await db.select().from(tasks).where(eq(tasks.user_id, userId!)));
+};
 
 const defaultBoards = ["Not Started", "In Progress", "Completed"];
 
 export default function TaskBoard() {
-  const { tasks } = useFireStore();
-  const [boards, setBoards] = useState<string[]>(defaultBoards);
+  const tasks = useLoaderData<typeof loader>();
+  const [boards] = useState<string[]>(defaultBoards);
 
   return (
     <div>
@@ -16,7 +28,7 @@ export default function TaskBoard() {
             <KanbanBoard
               name={board}
               key={board}
-              activities={tasks?.filter((task) => task.progress === board)}
+              activities={tasks.filter((task) => task.progress === board)}
             />
           );
         })}
