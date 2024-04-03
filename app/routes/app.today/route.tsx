71326443e4dc3e33e04727ede5d1a3461afc7156
kadeem-lewis <email@kadeemlewis.com>
2024-04-03 01:18:51 -0400

    added coming soon text to routes I havent started work on yet

--- app/routes/app.today/route.tsx
+++ app/routes/app.today/route.tsx
@@ -1,3 +1,7 @@
 export default function Today() {
-  return <div>Today</div>;
+  return (
+    <div className="text center flex  h-40 items-center justify-center text-3xl font-bold uppercase text-muted-foreground">
+      Coming Soon
+    </div>
+  );
 }
