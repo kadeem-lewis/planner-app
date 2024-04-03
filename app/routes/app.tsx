6fb6a213cc96e6c6c1ad033fec2ff3b4814f94c0
kadeem-lewis <email@kadeemlewis.com>
2024-04-03 01:08:33 -0400

    created action in app layout to handle creating new events or tasks

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -1,9 +1,16 @@
 import { Outlet } from "@remix-run/react";
 import NavBar from "~/components/main/NavBar";
 import SideBar from "~/components/main/SideBar";
-import { FirestoreProvider } from "~/contexts/FirestoreContext";
 import { getAuth } from "@clerk/remix/ssr.server";
-import { LoaderFunction, redirect } from "@remix-run/node";
+import {
+  ActionFunction,
+  LoaderFunction,
+  redirect,
+  json,
+} from "@remix-run/node";
+import { db } from "~/drizzle/config.server";
+import { events, tasks } from "~/drizzle/schema.server";
+import { ACTIVITY } from "~/components/constants/activities";
 
 export const meta = () => {
   return [
@@ -22,21 +29,80 @@ export const loader: LoaderFunction = async (args) => {
   return {};
 };
 
+export const action: ActionFunction = async (args) => {
+  const { userId } = await getAuth(args);
+  const formData = await args.request.formData();
+  console.log(
+    formData.get("activity"),
+    formData.get("title"),
+    formData.get("due_date"),
+    formData.get("progress"),
+    formData.get("description"),
+    formData.get("start-time"),
+    new Date(formData.get("end-time") as string).toISOString(),
+    formData.get("end-time"),
+  );
+
+  if (formData.get("activity") === ACTIVITY.EVENT) {
+    try {
+      await db.insert(events).values({
+        title: formData.get("title"),
+        start_date: new Date(formData.get("start-time") as string),
+        end_date: new Date(formData.get("end-time") as string),
+        user_id: userId,
+      });
+
+      return json({
+        status: 201,
+        activity: "event" as const,
+        success: true,
+      });
+    } catch (error) {
+      return json({
+        status: 500,
+        activity: "event" as const,
+        error: "Something went wrong",
+      });
+    }
+  }
+  if (formData.get("activity") === ACTIVITY.TASK) {
+    try {
+      await db.insert(tasks).values({
+        title: formData.get("title"),
+        description: formData.get("description"),
+        due_date: formData.get("due_date"),
+        user_id: userId,
+        progress: formData.get("progress"),
+        completed: formData.get("progress") === "Completed",
+      });
+      return json({
+        status: 201,
+        activity: "task" as const,
+        success: true,
+      });
+    } catch (error) {
+      return json({
+        status: 500,
+        activity: "task" as const,
+        error: "Something went wrong",
+      });
+    }
+  }
+};
+
 export default function AppLayout() {
   return (
     <>
-      <FirestoreProvider>
-        <NavBar />
-        <main className="grid min-h-screen grid-cols-10">
-          <SideBar />
+      <NavBar />
+      <main className="grid min-h-screen grid-cols-10">
+        <SideBar />
 
-          <div className="col-span-8">
-            <div className="p-4 lg:p-6">
-              <Outlet />
-            </div>
+        <div className="col-span-8">
+          <div className="p-4 lg:p-6">
+            <Outlet />
           </div>
-        </main>
-      </FirestoreProvider>
+        </div>
+      </main>
     </>
   );
 }
