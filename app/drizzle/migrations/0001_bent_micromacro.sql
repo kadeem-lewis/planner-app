6e1a838b955b4c50383a0d56b70e8e2f0106dfbb
kadeem-lewis <email@kadeemlewis.com>
2024-04-04 05:32:38 -0400

    deleted migrations

--- app/drizzle/migrations/0001_bent_micromacro.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS "events" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"title" text,
-	"start_date" date,
-	"end_date" date,
-	"created_at" timestamp DEFAULT now()
-);
--- app/drizzle/migrations/0003_foamy_plazm.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "events" ALTER COLUMN "start_date" SET DATA TYPE timestamp;--> statement-breakpoint
-ALTER TABLE "events" ALTER COLUMN "end_date" SET DATA TYPE timestamp;
