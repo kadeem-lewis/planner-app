7621e171d22c2d05c25b64792f66c8f92cc1ff61
kadeem-lewis <email@kadeemlewis.com>
2024-04-09 05:56:05 -0400

    added default classNames to Icon component to fix positioning

--- app/components/Icon.tsx
+++ app/components/Icon.tsx
@@ -1,10 +1,11 @@
 import spritesheetHref from "~/assets/icons/spritesheet.svg?url";
+import { cn } from "~/lib/utils";
 
 type IconProps = React.HTMLAttributes<SVGElement> & { name: string };
 
-export function Icon({ name, ...props }: IconProps) {
+export function Icon({ name, className, ...props }: IconProps) {
   return (
-    <svg {...props}>
+    <svg className={cn("inline self-center", className)} {...props}>
       <use href={`${spritesheetHref}#${name}`} />
     </svg>
   );
