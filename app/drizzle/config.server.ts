2ffafdbe14fa81779372ce5fa6a390a87a9e23a4
kadeem-lewis <email@kadeemlewis.com>
2024-04-11 05:45:36 -0400

    updated config to include drizzle schemas to allow querying

--- app/drizzle/config.server.ts
+++ app/drizzle/config.server.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+import * as schema from "./schema.server";
 
 const connectionString = process.env.DATABASE_URL;
 
@@ -10,4 +11,4 @@ if (!connectionString) {
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
 export const client = postgres(connectionString, { prepare: false });
-export const db = drizzle(client);
+export const db = drizzle(client, { schema });
